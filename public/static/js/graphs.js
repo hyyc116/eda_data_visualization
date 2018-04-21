b95e526032adb3045b480de484a0baf63aed9f28
hyyc116 <hyyc116@gmail.com>
2018-04-20 23:58:09 -0400

    YOUR ECONOMY COMPLETE

--- public/static/js/graphs.js
+++ public/static/js/graphs.js
@@ -4,6 +4,7 @@ queue().defer(d3.json, '/public/static/gejson/us-states.json').defer(d3.csv,
 function visualize(error, statesJson, yedata) {
 
 	var dateformat = d3.time.format("%Y");
+	var numberFormat = d3.format(".2f");
 	yedata.forEach(function(d) {
 		d['year'] = dateformat.parse(d["year"]);
 	});
@@ -27,62 +28,11 @@ function visualize(error, statesJson, yedata) {
 		return -p.value;
 	}
 	var min_state = stateGr.order(orderValue).top(1)[0].value;
-
-	// Charts
-	var us_map_chart = dc.geoChoroplethChart("#us-map-chart")
-	us_map_chart.width(600).height(400).dimension(stateDim).group(stateGr)
-			.colors(
-					[ "#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF",
-							"#51AEFF", "#36A2FF", "#1E96FF", "#0089FF",
-							"#0061B5" ]).colorDomain([ min_state, max_state ])
-			.overlayGeoJson(statesJson["features"], "state", function(d) {
-				return d.properties.name;
-			})
-			.projection(d3.geo.albersUsa().scale(800).translate([ 300, 200 ]))
-			.title(
-					function(p) {
-						return "State: " + p["key"] + "\n" + "Total Sales: "
-								+ Math.round(p["value"]) + " ";
-					});
-
-	var bbtAllGr = yearDim.group().reduceSum(function(d) {
-		return d['RESIDENT'] + d['NONRESIDENT'] + d['NONCOMMERCIAL'];
-	});
-	// 除了state之外的第二个图显示的是所有年份对应的Business的数量的bubble图
-
-	// //BUSINESS BY TYPE
-	function regroup(dim, cols) {
-		var _groupAll = dim.groupAll().reduce(function(p, v) { // add
-			cols.forEach(function(c) {
-				p[c] += +v[c];
-				// p.all.total += +v[c];
-			});
-
-			return p;
-		}, function(p, v) { // remove
-			cols.forEach(function(c) {
-				p[c] -= +v[c];
-				// p.all.total -= +v[c]
-			});
-
-			return p;
-		}, function() { // init
-			var p = {};
-			cols.forEach(function(c) {
-				p[c] = 0;
-			});
-			// p.all={}
-			// p.all.total=0;
-			return p;
-		});
-		return {
-			all : function() {
-				// or _.pairs, anything to turn the object into an array
-				return d3.map(_groupAll.value()).entries();
-			}
-		};
-	}
 	
+	// bubble图用的
+	var srSpbGr = yearDim.group().reduceSum(function(d) {
+		return +d['SALES PER BUSINESS']
+	});
 	// Define values (to be used in charts)
 	var minDate = yearDim.bottom(1)[0]["year"];
 	var maxDate = yearDim.top(1)[0]["year"];
@@ -96,51 +46,14 @@ function visualize(error, statesJson, yedata) {
 	var residentGr = yearDim.group().reduceSum(function(d){
 		return +d['RESIDENT'];
 	});
-	var residentGr = yearDim.group().reduceSum(function(d){
+	var nonresidentGr = yearDim.group().reduceSum(function(d){
 		return +d['NONRESIDENT'];
 	});
-	var residentGr = yearDim.group().reduceSum(function(d){
+	var nonCommercialGr = yearDim.group().reduceSum(function(d){
 		return +d['NONCOMMERCIAL'];
 	});
-	
-	var businessBarChart = dc.compositeChart('#business-bar-chart');
-	
-	businessBarChart.width(600).height(200).margins({
-		top : 100,
-		right : 50,
-		bottom : 50,
-		left : 100
-	}).transitionDuration(500)
-	.dimension(yearDim)
-	.group(bbtAllGr)
-	.elasticY(true)
-	.yAxisPadding(100)
-	.elasticX(true)
-	.xAxisPadding(500)
-	.x(d3.time.scale().domain([minDate, maxDate]))
-	.round(d3.time.year.round)
-	.xUnits(d3.time.year)
-	.stack(bbtAllGr, function(d){return d.value;})
-	.stack(bbtAllGr)
-	.brushOn(true)
-	.title(function(d) { return "Value: " + d.value; })
-    .renderTitle(true);
-	
-//	businessChart.
-//	.width(600).height(400).margins({
-//		top : 100,
-//		right : 50,
-//		bottom : 50,
-//		left : 100
-//	}).transitionDuration(500)
-//	.elasticY(true)
-//	.yAxisPadding(100)
-//	.elasticX(true)
-//	.compose()
-//	.brushOn(True)
-	
-	// // JOBS BY STAGE
-	//		
+
+//	
 	var jbsAllGr = yearDim.group().reduceSum(
 			function(d) {
 				return +d['SELF EMPLOYEE'] + +d['2-9 EMPLOYEES']
@@ -162,44 +75,7 @@ function visualize(error, statesJson, yedata) {
 	var jbs500Gr = yearDim.group().reduceSum(function(d) {
 		return +d['500+ EMPLOYEES'];
 	});
-	//		
-	//		
-	// //JOB CHANGES
-	// var jcGainedGr = yearDim.group().reduceSum(function(d) {
-	// return +d['jcgained']
-	// });
-	// var jcLostGr = yearDim.group().reduceSum(function(d) {
-	// return +d['jclost']
-	// });
-	// var jcNetGr = yearDim.group().reduceSum(function(d) {
-	// return +d['jcnet']
-	// });
-	//		
-	// //Sale/Revenue
-	// var srAllGr = yearDim.group().reduceSum(function(d) {
-	// return +d['srall']
-	// });
-	// var srSpeGr = yearDim.group().reduceSum(function(d) {
-	// return +d['srspe']
-	// });
-	var srSpbGr = yearDim.group().reduceSum(function(d) {
-		return +d['SALES PER BUSINESS']
-	});
-
-	var all = ndx.groupAll();
-
 	
-
-	var max_color = srSpbGr.top(1)[0].value;
-	var min_color = srSpbGr.order(orderValue).top(1)[0].value;
-	// console.log(minDate);
-	// console.log(maxDate);
-
-	var combine_chart = dc.bubbleChart("#combine_chart");
-	//
-	// // var jc_composite = dc.compositeChart("#changes-chart");
-	// // var sr_composite = dc.compositeChart("#sales-chart");
-	//
 	var bubbleGr = yearDim.group().reduce(
 			// add
 			function(p, v) {
@@ -303,9 +179,234 @@ function visualize(error, statesJson, yedata) {
 	var min_br = bubbleGr.order(ravg_order_r).top(1)[0].value.ravg;
 	var min_by = bubbleGr.order(yavg_order_r).top(1)[0].value.yavg;
 	var min_bc = bubbleGr.order(cavg_order_r).top(1)[0].value.cavg;
-	combine_chart.width(600).height(400).margins({
+	
+
+	var max_color = srSpbGr.top(1)[0].value;
+	var min_color = srSpbGr.order(orderValue).top(1)[0].value;
+	
+	var gainOrLoss = ndx.dimension(function(d) {
+		 return +d['GAINED'] > +d['LOST'] ? "GAIN" : "LOST";
+		 });
+		 var gainOrLossGroup = gainOrLoss.group();
+	
+	var all = ndx.groupAll();
+
+	// Charts
+	var us_map_chart = dc.geoChoroplethChart("#us-map-chart");
+	us_map_chart.width(600).height(400).dimension(stateDim).group(stateGr)
+			.colors(
+					[ "#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF",
+							"#51AEFF", "#36A2FF", "#1E96FF", "#0089FF",
+							"#0061B5" ]).colorDomain([ min_state, max_state ])
+			.overlayGeoJson(statesJson["features"], "state", function(d) {
+				return d.properties.name;
+			})
+			.projection(d3.geo.albersUsa().scale(800).translate([ 300, 200 ]))
+			.title(
+					function(p) {
+						return "State: " + p["key"] + "\n" + "Total Sales: "
+								+ Math.round(p["value"]) + " ";
+					});
+
+
+	// //BUSINESS BY TYPE
+	function regroup(dim, cols) {
+		var _groupAll = dim.groupAll().reduce(function(p, v) { // add
+			cols.forEach(function(c) {
+				p[c] += +v[c];
+				// p.all.total += +v[c];
+			});
+
+			return p;
+		}, function(p, v) { // remove
+			cols.forEach(function(c) {
+				p[c] -= +v[c];
+				// p.all.total -= +v[c]
+			});
+
+			return p;
+		}, function() { // init
+			var p = {};
+			cols.forEach(function(c) {
+				p[c] = 0;
+			});
+			// p.all={}
+			// p.all.total=0;
+			return p;
+		});
+		return {
+			all : function() {
+				// or _.pairs, anything to turn the object into an array
+				return d3.map(_groupAll.value()).entries();
+			}
+		};
+	}
+	
+	var businessBarChart = dc.barChart('#business-bar-chart');
+	
+	businessBarChart.width(800).height(150).margins({
+		top : 50,
+		right : 100,
+		bottom : 50,
+		left : 100
+	}).transitionDuration(500)
+	.dimension(yearDim)
+	.group(bbtAllGr)
+	.elasticY(true)
+	.yAxisPadding(100)
+	.elasticX(true)
+	.xAxisPadding(500)
+	.x(d3.time.scale().domain([minDate, maxDate]))
+	.round(d3.time.year.round)
+	.xUnits(d3.time.years)
+	.centerBar(true)
+	.brushOn(false)
+	.title(function(d) { return "Value: " + d.value; })
+    .renderTitle(true)
+    .yAxis().ticks(3);
+	
+	var businessChart = dc.compositeChart('#business-chart');
+	businessChart
+	.width(800).height(400).margins({
 		top : 100,
-		right : 50,
+		right : 100,
+		bottom : 60,
+		left : 100
+	}).transitionDuration(500)
+	.dimension(yearDim)
+	.group(residentGr)
+	.elasticY(true)
+	.yAxisPadding(100)
+	.elasticX(true)
+	.yAxisLabel("Number of Business")
+    .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+    .x(d3.time.scale().domain([minDate, maxDate]))
+	.compose([
+		dc.lineChart(businessChart)
+        .dimension(yearDim)
+        .colors('green')
+        .renderArea(true)
+        .group(residentGr, "RESIDENT"),
+		
+        dc.lineChart(businessChart)
+        .dimension(yearDim)
+        .renderArea(true)
+        .group(nonresidentGr, "NONRESIDENT"),
+        
+        dc.lineChart(businessChart)
+        .dimension(yearDim)
+        .colors('red')
+        .renderArea(true)
+        .group(nonCommercialGr, "NONCOMMERCIAL")
+		
+	]);
+	
+	// // JOBS BY STAGE
+	
+var jobsBarChart = dc.barChart('#jobs-bar-chart');
+	
+jobsBarChart.width(800).height(150).margins({
+		top : 50,
+		right : 100,
+		bottom : 50,
+		left : 100
+	}).transitionDuration(500)
+	.dimension(yearDim)
+	.group(jbsAllGr)
+	.elasticY(true)
+	.yAxisPadding(100)
+	.elasticX(true)
+	.xAxisPadding(500)
+	.x(d3.time.scale().domain([minDate, maxDate]))
+	.round(d3.time.year.round)
+	.xUnits(d3.time.years)
+	.centerBar(true)
+	.brushOn(false)
+	.title(function(d) { return "Value: " + d.value; })
+    .renderTitle(true)
+    .yAxis().ticks(3);
+	
+	var jobsChart = dc.compositeChart('#jobs-chart');
+	jobsChart
+	.width(800).height(400).margins({
+		top : 120,
+		right : 100,
+		bottom : 60,
+		left : 100
+	}).transitionDuration(500)
+	.dimension(yearDim)
+	.group(jbsAllGr)
+	.elasticY(true)
+	.yAxisPadding(100)
+	.elasticX(true)
+	.yAxisLabel("Number of Business")
+    .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+    .x(d3.time.scale().domain([minDate, maxDate]))
+	.compose([
+        
+        dc.lineChart(jobsChart)
+        .dimension(yearDim)
+        .colors('red')
+        .renderArea(true)
+        .group(jbs10Gr, "10-99 EMPLOYEES"),
+        
+        dc.lineChart(jobsChart)
+        .dimension(yearDim)
+        .colors('pink')
+        .renderArea(true)
+        .group(jbs100Gr, "100-499 EMPLOYEES"),
+		
+        dc.lineChart(jobsChart)
+        .dimension(yearDim)
+        .renderArea(true)
+        .colors('gray')
+        .group(jbs500Gr, "500+ EMPLOYEES"),
+        
+        dc.lineChart(jobsChart)
+        .dimension(yearDim)
+        .colors('green')
+        .renderArea(true)
+        .group(jbs1Gr, "SELF EMPLOYEE"),
+        
+        dc.lineChart(jobsChart)
+        .dimension(yearDim)
+        .renderArea(true)
+        .group(jbs2Gr, "2-9 EMPLOYEES")
+		
+	]);
+	//		
+	//		
+	// //JOB CHANGES
+	// var jcGainedGr = yearDim.group().reduceSum(function(d) {
+	// return +d['jcgained']
+	// });
+	// var jcLostGr = yearDim.group().reduceSum(function(d) {
+	// return +d['jclost']
+	// });
+	// var jcNetGr = yearDim.group().reduceSum(function(d) {
+	// return +d['jcnet']
+	// });
+	//		
+	// //Sale/Revenue
+	// var srAllGr = yearDim.group().reduceSum(function(d) {
+	// return +d['srall']
+	// });
+	// var srSpeGr = yearDim.group().reduceSum(function(d) {
+	// return +d['srspe']
+	// });
+	
+	// console.log(minDate);
+	// console.log(maxDate);
+
+	var combine_chart = dc.bubbleChart("#combine_chart");
+	//
+	// // var jc_composite = dc.compositeChart("#changes-chart");
+	// // var sr_composite = dc.compositeChart("#sales-chart");
+	//
+	
+	combine_chart.width(1800).height(400).margins({
+		top : 20,
+		right : 200,
 		bottom : 50,
 		left : 100
 	}).transitionDuration(500).dimension(yearDim).group(bubbleGr)
@@ -323,102 +424,35 @@ function visualize(error, statesJson, yedata) {
 			function(d, i) {
 				return +d.value.cavg;
 			})
-	.maxBubbleRelativeSize(0.2)
-	
+	.maxBubbleRelativeSize(0.05)
+	.xAxisLabel('Sales Per Employee')
+	.yAxisLabel('Sales Per Business')
 	.x(d3.scale.linear().domain([ min_bx, max_bx ]))
 	.y(d3.scale.linear().domain([ min_by, max_by ]))
-	.r(d3.scale.log().domain([ min_br, max_bx ]))
+	.r(d3.scale.log().domain([ 10, 40 ]))
 	.elasticRadius(true)
 	.sortBubbleSize(true)
 	.elasticY(true)
-			.yAxisPadding(max_by/3).elasticX(true).xAxisPadding(max_bx/3)
+			.yAxisPadding(300).elasticX(true).xAxisPadding(300)
 			.renderHorizontalGridLines(true).renderVerticalGridLines(true)
 			.renderLabel(true).renderTitle(true).label(function(p) {
 				return p.key.getFullYear();
 			}).title(function(p) {
 //				console.log(p);
-				return p.value;
+				return p.key.getFullYear()
+                + "\n"
+                + "Index Gain: " + numberFormat(p.value.cavg) + "\n"
+                + "Sales Per Employee: " + numberFormat(p.value.x) + "\n"
+                + "Sales Per Business: " + numberFormat(p.value.yavg) + "\n"
+                + "Total Jobs:" + numberFormat(p.value.rtotal);
 			});
 
-	//
-	// // we need access to the entire rows (or at least all the
-	// // columns we're using), so just make the dimension function
-	// // a pass-through. the keys aren't going to make much sense
-	// // but we don't care.
-	// var dim = ndx.dimension(function(r) {
-	// return r;
-	// });
-	// var bbtPirGr = regroup(dim,
-	// [ 'bbtresident', 'bbtnonresident', 'bbtnoncom' ]);
-	//
-	// var sidewaysRow = dc.rowChart('#business-chart').width(400).height(200)
-	// .dimension(dim).group(bbtPirGr)
-	// // .legend(dc.legend())
-	// .label(function(b) {
-	// // console.log(b);
-	// if (b.key == "bbtnoncom") {
-	// return "NONCOMMERCIAL";
-	// } else if (b.key == "bbtnonresident") {
-	// return "NONRESIDENT";
-	// } else {
-	// return "RESIDENT";
-	// }
-	// });
-	//
-	// sidewaysRow.filterHandler(function(dim, filters) {
-	// if (filters && filters.length)
-	// dim.filterFunction(function(r) {
-	// return filters.some(function(c) {
-	// return r[c] > 0;
-	// });
-	// })
-	// else
-	// dim.filterAll();
-	// return filters;
-	// });
-	//
-	// var jbsPirGr = regroup(dim, [ 'jbs1', 'jbs2', 'jbs10', 'jbs100', 'jbs500'
-	// ]);
-	//
-	// sidewaysRow =
-	// dc.rowChart('#jobs-chart').width(400).height(200).dimension(
-	// dim).group(jbsPirGr)
-	// // .legend(dc.legend())
-	// .label(function(b) {
-	// // console.log(b);
-	// if (b.key == "jbs1") {
-	// return "SELF EMPLOYED (1)";
-	// } else if (b.key == "jbs2") {
-	// return "2-9 Employees";
-	// } else if (b.key == 'jbs10') {
-	// return "10-99 Employees";
-	// } else if (b.key == 'jbs100') {
-	// return "100-499 Employees";
-	// } else {
-	// return "500+ Employees";
-	// }
-	// });
-	//
-	// sidewaysRow.filterHandler(function(dim, filters) {
-	// if (filters && filters.length)
-	// dim.filterFunction(function(r) {
-	// return filters.some(function(c) {
-	// return r[c] > 0;
-	// });
-	// })
-	// else
-	// dim.filterAll();
-	// return filters;
-	// });
-	//
-	// var gainOrLoss = ndx.dimension(function(d) {
-	// return +d.jcgained > +d.jclost ? "GAIN" : "LOST";
-	// });
-	// var gainOrLossGroup = gainOrLoss.group();
-	// var glChart = dc.pieChart("#gl-chart").width(800).height(200).dimension(
-	// gainOrLoss).group(gainOrLossGroup).label(function(d) {
-	// return d.key + "(" + (d.value / all.value() * 100).toFixed(1) + "%)";
-	// });
+	
+	 
+	 var glChart = dc.pieChart("#gl-chart").width(800).height(400).dimension(
+	 gainOrLoss).group(gainOrLossGroup).label(function(d) {
+	 return d.key + "(" + (d.value / all.value() * 100).toFixed(1) + "%)";
+	 });
 
 	// / 实验 使用地图的引动其他的图的变化，但是其他的图不引动地图的变化
 
